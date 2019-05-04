6986be9fc67f531051ed7d90e1010fdd2596fdc7
vagrant <vagrant@vagrant.vm>
2019-05-04 12:09:24 +0000

    3本購入まで対応

--- bonus_drink.rb
+++ bonus_drink.rb
@@ -1,9 +1,8 @@
 class BonusDrink
   def self.total_count_for(amount)
-    if amount == 0
-      0
-    else
-      1
-    end
+    amount += self.bonus_count_for(amount)
+  end
+  def self.bonus_count_for(amount)
+    amount / 3
   end
 end
