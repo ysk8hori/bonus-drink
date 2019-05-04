73f271cea082330e323c1ad046837bad0a6c4201
vagrant <vagrant@vagrant.vm>
2019-05-04 13:09:54 +0000

    11本購入に対応

--- bonus_drink.rb
+++ bonus_drink.rb
@@ -3,6 +3,14 @@ class BonusDrink
     amount += self.bonus_count_for(amount)
   end
   def self.bonus_count_for(amount)
-    amount / 3
+    # 与えられた本数から求められるボーナスの本数
+    bonus_count = amount / 3
+    # ボーナスに変えられなかった余りの本数
+    remainder = amount % 3
+    if 3 <= bonus_count + remainder
+      # ボーナスの本数＋余りの本数が3以上の場合は合算してさらにボーナスをもらう（再帰的呼び出し）
+      bonus_count += self.bonus_count_for(bonus_count + remainder)
+    end
+    bonus_count
   end
 end
--- spec/bonus_drink_spec.rb
+++ spec/bonus_drink_spec.rb
@@ -4,4 +4,5 @@ describe BonusDrink do
   specify { expect(BonusDrink.total_count_for(0)).to eq 0 }
   specify { expect(BonusDrink.total_count_for(1)).to eq 1 }
   specify { expect(BonusDrink.total_count_for(3)).to eq 4 }
+  specify { expect(BonusDrink.total_count_for(11)).to eq 16 }
 end
